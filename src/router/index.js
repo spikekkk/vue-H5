6f59d7b643e8d47d021f22c12e1fa11fe58eb1c5
dongdong <690555367@qq.com>
2018-03-13 18:06:52 +0800

    login 页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [{
+  path: '/',
+  redirect:"/login",
+},{
+  path:"/login",
+  name:"login",
+  component: resolve => require(["../components/login.vue"],resolve)
+}, {
+  path: "/home",
+  name: "home",
+  component: resolve => require(["../components/home.vue"], resolve),
+//   children:[{
+//     path:"/",
+//     component:resolve => require(["../components/pages/index.vue"],resolve)
+//   }]
+}]
+
+const router = new Router({
+  mode: 'history',
+  routes: routes
 })
+
+export default router
