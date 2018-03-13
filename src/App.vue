b6f054da52f6854c3b54b5b7522c409195d3e55a
dongdong <690555367@qq.com>
2018-03-13 18:06:52 +0800

    login 页面

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+	<div id="app">
+		<router-view/>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+	export default {
+		name: 'App'
+	}
 </script>
 
 <style>
-
+	@import url("../static/css/common.css");
 </style>
