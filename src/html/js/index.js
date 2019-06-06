c05ee95070c7be3fd27131dfb5284766866610af
kkk <sunkai@ishanshan.com>
2019-06-06 14:29:59 +0800

    H5初步删减

--- src/html/js/index.js
+++
@@ -1,5 +0,0 @@
-// 动态设置html的fontSize的数值
-var fontSize = window.screen.width / 10 + "px";
-// 1rem = 37.5px;
-// console.log(window.screen.width);
-document.getElementsByTagName("html")[0].style.fontSize = fontSize;
